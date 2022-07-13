ae05b5a35df902071a753b43f554ddb5da2728b6
JupiterPi <jupiterpi@outlook.de>
2022-07-13 20:27:35 +0200

    base

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const http = require('http');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({});
+
+const hosts = JSON.parse(fs.readFileSync("hosts.json"));
+console.log(hosts);
+
+http.createServer(function(req, res) {
+    const host = req.headers["host"];
+    for (let h of hosts) {
+        if (h["host"] == host) {
+            proxy.web(req, res, { target: h["target"] });
+        }
+    }
+}).listen(80);
