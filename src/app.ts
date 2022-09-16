d8e6523e032eaec48ad52629acade03280d3e78a
JupiterPi <jupiterpi@outlook.de>
2022-09-16 21:19:03 +0200

    full working version

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,28 @@ const fs = require("fs");
 
 const http = require('http');
 const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({
+    proxyTimeout: 500
+});
 
 const hosts = JSON.parse(fs.readFileSync("hosts.json"));
 console.log(hosts);
 
+proxy.on("error", function (err, req, res) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("System unavailable: " + err);
+});
+
 http.createServer(function(req, res) {
-    const host = req.headers["host"];
-    for (let h of hosts) {
-        if (h["host"] == host) {
-            proxy.web(req, res, { target: h["target"] });
+    try {
+        const host = req.headers["host"];
+        console.log(host);
+        for (let h of hosts) {
+            if (h["host"] == host) {
+                proxy.web(req, res, {target: h["target"]});
+            }
         }
+    } catch (e) {
+        console.error(e.message);
     }
 }).listen(63000);
